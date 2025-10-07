462437798975ce854a9c9550474ade793b1abff2
Tars <112401020@smail.iitpkd.ac.in>
2025-10-07 16:48:58 +0530

    hh

---
+++ lib/machine.ml
@@ -0,0 +1,9 @@
+let storage = function
+            | M    -> 0b100
+            | D    -> 0b010
+            | A    -> 0b001 
+            | MD   -> 0b110
+            | AM   -> 0b101
+            | AD   -> 0b011
+            | AMD  -> 0b111
+
