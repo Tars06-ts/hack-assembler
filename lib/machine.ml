26753210795bfdaa0b2366549e5d3aaad812ca57
Tars <112401020@smail.iitpkd.ac.in>
2025-10-14 16:52:40 +0530

    encoder stuff

--- lib/machine.ml
+++ lib/machine.ml
@@ -1,9 +1,35 @@
-(* let storage = function
-            | M    -> 0b100
-            | D    -> 0b010
-            | A    -> 0b001 
-            | MD   -> 0b110
-            | AM   -> 0b101
-            | AD   -> 0b011
-            | AMD  -> 0b111
-*)
+(* -- Hack assembly to binary --  *)
+
+(*Jump Module*)
+module Jmp = struct
+    let jmp (j: Ast.Instr.jinst) : int list =
+        match j with 
+                | JGT -> [ 0; 0; 1]
+                | JEQ -> [ 0; 1; 0]
+                | JGE -> [ 0; 1; 1]
+                | JLT -> [ 1; 0; 0]
+                | JNE -> [ 1; 0; 1]
+                | JLE -> [ 1; 1; 0]
+                | JMP -> [ 1; 1; 1]
+end
+
+(*Destination module*)
+module Dest = struct
+    let dest (d: Ast.Reg.r) : int list =
+        match d with 
+                | M -> [0; 0; 1]
+                | D -> [0; 1; 0]
+                | A -> [1; 0; 0]
+end
+
+(*Constant module*)
+module Const = struct
+    let const (c: Ast.Instr.const) : int list = 
+        match c with
+                | Zero     -> [0;1;0;1;0;1;0]
+                | One      -> [0;1;1;1;1;1;1]
+                | MinusOne -> [0;1;1;1;0;1;0] 
+end
+                             
+
+
