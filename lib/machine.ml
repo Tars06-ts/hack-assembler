c33cfabb251bdbe69661eb869dd2168e5e21630a
Tars <112401020@smail.iitpkd.ac.in>
2025-10-21 13:46:50 +0530

    machine 1

--- lib/machine.ml
+++ lib/machine.ml
@@ -7,3 +7,12 @@ let storage = function
             | AD   -> 0b011
             | AMD  -> 0b111
 
+let jmp = function
+        | JGT -> 0b100
+        | JEQ -> 0b010
+        | JGE -> 0b110
+        | JLT -> 0b001
+        | JNE -> 0b101
+        | JLE -> 0b011
+        | JMP -> 0b111 
+
