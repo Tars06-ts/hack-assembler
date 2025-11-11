14471adde8a3b8965ffb8d28c9a208a28c44f9d5
Tars <112401020@smail.iitpkd.ac.in>
2025-11-11 16:50:53 +0530

    mach

--- lib/machine.ml
+++ lib/machine.ml
@@ -101,17 +101,13 @@ let bin_convert (n : int) : string =
   let rec to_binary_string acc i =
     if i < 0 then acc (* Stop condition when we've processed all 15 bits (i=-1) *)
     else
-      (* Create a mask with a single bit set at position i *)
       let mask = 1 lsl i in
       
-      (* Check if the i-th bit is set in the number n *)
       let bit_val = if (n land mask) <> 0 then '1' else '0' in
       
-      (* Recursively call, prepending the new bit character to the accumulator string *)
       to_binary_string (acc ^ (String.make 1 bit_val)) (i - 1)
   in
   
-  (* Start the iteration from the 14th bit (the 15th bit position, counting from 0) *)
   to_binary_string "" 14
    
 module Inst = struct
